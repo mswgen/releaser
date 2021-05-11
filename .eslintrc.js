d1edf21a6df66a8ad341d5ecba607c146675cfeb
mswgen <54498184+mswgen@users.noreply.github.com>
2021-05-11 23:10:25 +0900

    style(.eslintrc.js): 배열 정의 스타일 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,7 @@ module.exports = {
     'es2021': true,
     'node': true
   },
-  'extends': [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended'
-  ],
+  'extends': ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   'parser': '@typescript-eslint/parser',
   'parserOptions': {
     'ecmaVersion': 12,
@@ -14,21 +11,9 @@ module.exports = {
   },
   'plugins': ['@typescript-eslint'],
   'rules': {
-    'indent': [
-      'error',
-      2
-    ],
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'never'
-    ]
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'never']
   }
 }
